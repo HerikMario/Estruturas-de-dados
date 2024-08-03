c29a87859d7361f5145479b72f882231b798c58b
Herik Mario <herikmariomr@gmail.com>
2024-08-02 22:52:55 -0300

    add: funcao inverter array

---
+++ Ponteiros/Exercícios aula 06/inverter.cpp
@@ -0,0 +1,59 @@
+/*Escreva uma função "void inverter (double *p, int n)" que inverta a ordem dos "n" elementos do vetor (cujo primeiro elemento é) apontado por "p", ou seja, o último número deve passar a estar na primeira posição, o penúltimo na segunda, etc.*/
+
+#include <iostream>
+
+using namespace std;
+
+void inverter (double *p, int n) {
+
+    double* a = new double[n];
+
+    for (int i = 0; i < n ; ++i) {
+        a[(n-1) - i] = *(p+i);
+    }
+
+    for (int i = 0; i < n; ++i) {
+        *(p+i) = a[i]; 
+    }
+
+}
+
+void preencher_array(double* o, int tam) {
+
+    for (int i = 0; i < tam; ++i) {
+        o[i] = i+1;
+    }
+
+}
+
+int main() {
+
+    int tamanho;
+
+    cout << "Escolha o tamanho da array: ";
+
+    cin >> tamanho;
+
+    double* u = new double[tamanho];
+
+    preencher_array(u,tamanho);
+
+    cout << "Array antes: ";
+
+    for (int i = 0; i < tamanho; ++i){
+        cout << u[i] << " ";
+    }
+
+    cout << endl;
+
+    inverter(u,tamanho);
+
+    cout << "Array invertida: ";
+
+    for (int i = 0; i < tamanho ; ++i) {
+        cout << u[i] << " ";
+    }
+
+    cout << endl;
+
+}
